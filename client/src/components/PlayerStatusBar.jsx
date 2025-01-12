b56eb1e6a188959fd4d31b4e1cbcd60fb14f16c5
skrom71 <skrom70@gmail.com>
2025-01-12 11:54:24 +0200

    UI

--- client/src/components/PlayerStatusBar.jsx
+++ client/src/components/PlayerStatusBar.jsx
@@ -1,14 +1,33 @@
 import { useState, useEffect } from "react";
 import "./PlayerStatusBar.css";
+import BackCardIcon from "/src/assets/images/card-back.png";
 
 // Функция для получения случайного изображения
 function getRandomCardImage() {
   // Генерируем случайное число от 1 до 52
-  const randomNumber = Math.floor(Math.random() * 52) + 1;
+
+  const rangs = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+  ];
+  const suits = ["D", "H", "S", "C"];
+  const randomRandIndex = Math.floor(Math.random() * rangs.length);
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
 
   // Формируем имя файла
-  const imageName = `card-${randomNumber}.png`;
-
+  const imageName = `${rangs[randomRandIndex]}${suits[randomSuitIndex]}.png`;
+  console.log("Случайный элемент:", imageName);
   // Путь к изображению (можно указать папку, если изображения там хранятся)
   const imagePath = `/src/assets/images/${imageName}`;
 
@@ -17,17 +36,30 @@ function getRandomCardImage() {
 
 const link = getRandomCardImage();
 
-function PlayerStatusBar() {
-  return (
-    <div className="main-container-status-bar">
-      <img src={getRandomCardImage()} className="player-status-bar-card1"></img>
-      <img src={getRandomCardImage()} className="player-status-bar-card2"></img>
-      <div className="player-status-bar-footer">
-        <div className="player-status-bar-name">Player</div>
-        <div className="player-status-bar-balance">9543.3</div>
+function PlayerStatusBar({ isCurrent }) {
+  function renderTag() {
+    let card1Path;
+    let card2Path;
+    if (isCurrent) {
+      card1Path = getRandomCardImage();
+      card2Path = getRandomCardImage();
+    } else {
+      card1Path = BackCardIcon;
+      card2Path = BackCardIcon;
+    }
+    return (
+      <div className="main-container-status-bar">
+        <img src={card1Path} className="player-status-bar-card1"></img>
+        <img src={card2Path} className="player-status-bar-card2"></img>
+        <div className="player-status-bar-footer">
+          <div className="player-status-bar-name">Player</div>
+          <div className="player-status-bar-balance">9543.3</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return renderTag(isCurrent);
 }
 
 export default PlayerStatusBar;
--- client/src/pages/holdem/HoldemSession.jsx
+++ client/src/pages/holdem/HoldemSession.jsx
@@ -3,39 +3,89 @@ import io from "socket.io-client";
 import "./HoldemSession.css";
 import PlayerStatusBar from "../../components/PlayerStatusBar.jsx";
 import JoinButton from "../../components/buttons/PokerTableJoinButton.jsx";
+import ChipIcon from "/src/assets/images/chip-1.png";
+
+// Функция для получения случайного изображения
+function getRandomCardImage() {
+  // Генерируем случайное число от 1 до 52
+
+  const rangs = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+  ];
+  const suits = ["D", "H", "S", "C"];
+  const randomRandIndex = Math.floor(Math.random() * rangs.length);
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+
+  // Формируем имя файла
+  const imageName = `${rangs[randomRandIndex]}${suits[randomSuitIndex]}.png`;
+  console.log("Случайный элемент:", imageName);
+  // Путь к изображению (можно указать папку, если изображения там хранятся)
+  const imagePath = `/src/assets/images/${imageName}`;
+
+  return imagePath;
+}
 
 function HoldemSession() {
   return (
     <div className="main-container-session">
       <div className="player player-top-1">
-        <PlayerStatusBar />
+        <PlayerStatusBar isCurrent={true} />
       </div>
+      <div className="player-top-1-bet">34.4</div>
       <div className="player player-top-2">
         <JoinButton />
       </div>
       <div className="player player-left-1">
-        <PlayerStatusBar />
+        <PlayerStatusBar isCurrent={false} />
       </div>
-      <div className="player player-left-2">
+      <div className="player-left-1-bet">34334.4</div>
+      {/* <div className="player player-left-2">
         <JoinButton />
-      </div>
+      </div> */}
       <div className="player player-left-3">
-        <PlayerStatusBar />
+        <PlayerStatusBar isCurrent={false} />
       </div>
       <div className="player player-right-1">
-        <PlayerStatusBar />
+        <PlayerStatusBar isCurrent={false} />
       </div>
-      <div className="player player-right-2">
+      {/* <div className="player player-right-2">
         <JoinButton />
-      </div>
+      </div> */}
       <div className="player player-right-3">
-        <PlayerStatusBar />
+        <PlayerStatusBar isCurrent={false} />
       </div>
       <div className="player player-bottom-1">
-        <PlayerStatusBar />
+        <PlayerStatusBar isCurrent={false} />
       </div>
-      <div className="player player-bottom-2">
+      {/* <div className="player player-bottom-2">
         <JoinButton />
+      </div> */}
+
+      <div className="pot">
+        <img src={ChipIcon} className="pot-icon" />
+        234.3
+      </div>
+
+      <div className="blinde">Min.Max: 10/20</div>
+
+      <div className="commun-card-container">
+        <img src={getRandomCardImage()} className="commun-card"></img>
+        <img src={getRandomCardImage()} className="commun-card"></img>
+        <img src={getRandomCardImage()} className="commun-card"></img>
+        <img src={getRandomCardImage()} className="commun-card"></img>
+        <img src={getRandomCardImage()} className="commun-card"></img>
       </div>
     </div>
   );
